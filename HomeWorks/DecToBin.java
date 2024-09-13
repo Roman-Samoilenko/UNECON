b49bcf12247e960cb0d16b2dd7a0ec94537a0d45
Roman Samoilenko <romasampro@gmail.com>
2024-09-13 14:59:13 +0300

    1

---
+++ HomeWorks/DecToBin.java
@@ -0,0 +1,19 @@
+package UNECON.HomeWorks;
+
+import java.util.Scanner;
+
+public class DecToBin {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    long n = sc.nextLong();
+    if(n < 0){
+        System.out.println(0);
+    }
+    String x = "";
+    while(n > 0){
+        x = n%2+ x;
+        n /= 2;
+    }
+    System.out.println(x);
+    }
+}
---
+++ HomeWorks/test.java
@@ -0,0 +1,12 @@
+package UNECON.HomeWorks;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 1;
+
+        while (a > 0){
+            a++;
+        }
+        System.out.println(a-1);
+    }
+}
